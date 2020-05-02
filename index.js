7ad1f52b9b03888753f4856bd3f4823ff8c493bf
Maxim Borovik <48134013+mouxez@users.noreply.github.com>
2020-05-02 20:53:08 +0300

    remove api token

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const TelegramBot = require('node-telegram-bot-api');
-const token = '946921278:AAGk69mnPplCOpXHozlDqvfzDs85pHOiRr4';
+const token = '';
 
 const bot = new TelegramBot(token, {polling: true});
